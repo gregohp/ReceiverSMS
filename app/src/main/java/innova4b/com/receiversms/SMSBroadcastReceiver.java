fc0bd200532e2253f2f30ac6f724506007a14b05
gregohp <grego@hemen.eu>
2017-06-26 19:12:17 +0200

    SMSBroadcastReceiver finished

--- app/src/main/java/innova4b/com/receiversms/SMSBroadcastReceiver.java
+++ app/src/main/java/innova4b/com/receiversms/SMSBroadcastReceiver.java
@@ -3,7 +3,13 @@ package innova4b.com.receiversms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SMSBroadcastReceiver extends BroadcastReceiver {
     final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
@@ -11,6 +17,20 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (SMS_RECEIVED.equals(intent.getAction())){
             Bundle bundle = intent.getExtras();
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            String format = bundle.getString("format");
+
+            final SmsMessage[] messages = new SmsMessage[pdus.length];
+
+            for (int i = 0; i < pdus.length ; i++) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                Toast.makeText(context, messages[i].getMessageBody(), Toast.LENGTH_LONG).show();
+            }
+
 
         }
     }
