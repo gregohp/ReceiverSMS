dea06f4174cdfd2b8ebbbbd61682cbdc84bbd62c
gregohp <grego@hemen.eu>
2017-06-29 20:27:54 +0200

    SMSBroadcastReceiver finished

--- app/src/main/java/innova4b/com/receiversms/SMSBroadcastReceiver.java
+++ app/src/main/java/innova4b/com/receiversms/SMSBroadcastReceiver.java
@@ -1,11 +1,16 @@
 package innova4b.com.receiversms;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.telephony.SmsMessage;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -29,9 +34,29 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
                     messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                 }
                 Toast.makeText(context, messages[i].getMessageBody(), Toast.LENGTH_LONG).show();
+                showNotification(context, messages[i].getMessageBody());
             }
 
 
         }
+    }
+
+    private void showNotification(Context context, String msg) {
+
+        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher);
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle("Big Content Title")
+                .addLine("Message:")
+                .addLine(msg);
+        notificationBuilder
+                .setStyle(inboxStyle)
+                .setSound(notificationSoundUri);
+
+        NotificationManager notificationManager =(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+
     }
 }
